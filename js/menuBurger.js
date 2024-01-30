b61d2ca5359b100be94edc09b6eebc667ae1b763
Souyris Audrey <lilo-jolie@hotmail.fr>
2024-01-30 16:44:55 +0000

    menu fonctionnel à reprendre: media queries du banner

--- js/menuBurger.js
+++ js/menuBurger.js
@@ -3,22 +3,21 @@ console.log(
   " le script javascript lancé se nomme menu_burger.js et il est fonctionnel "
 );
 
-//gestion ouverture menu
-
-/*Ouverture menu*/
-
+// Sélection des éléments du DOM
 const boutonMenu = document.querySelector(".boutonMenu");
 const burger = document.querySelector(".MenuFull");
-
-boutonMenu.addEventListener("click", () => {
-  burger.classList.toggle("nav_open");
-  boutonMenu.classList.toggle("active");
-});
-
-/*Fermeture menu sur click lien*/
-
 const menuLinks = document.querySelectorAll(".MenuFull ul li a");
 
+// Fonction pour basculer l'état du menu et du bouton
+const toggleMenu = () => {
+  burger.classList.toggle("nav_open");
+  boutonMenu.classList.toggle("active");
+};
+
+// Gestion de l'ouverture du menu au clic sur le bouton
+boutonMenu.addEventListener("click", toggleMenu);
+
+// Gestion de la fermeture du menu au clic sur un lien
 menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     burger.classList.remove("nav_open");
