b3287025d8fd183e5aee60ad50305bc7c1352da8
Souyris Audrey <lilo-jolie@hotmail.fr>
2024-02-08 22:42:55 +0000

    appel du js modifié

--- functions.php
+++ functions.php
@@ -29,9 +29,9 @@ function script_JS_Custo()
 {
     // wp_enqueue_script('fadein-script', get_stylesheet_directory_uri() . '/js/scroll-fadePage.js', array('jquery'), '1.0', true);
     // appel de la page du flottement du logo:
-    wp_enqueue_script('flottement-logo-script', get_stylesheet_directory_uri() . './js/FlottementLogo.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('flottement-logo-script', get_stylesheet_directory_uri() . '/js/FlottementLogo.js', array('jquery'), '1.0', true);
     // Gestion des Animation des Titres
-    wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . './js/animationTitre.js', array(), '1.0.0', true);
+    wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/js/animationTitre.js', array(), '1.0.0', true);
     // Parallaxe
     wp_enqueue_script('parallaxe-script', get_stylesheet_directory_uri() . '/js/Parallaxe.js', array('jquery'), '1.0', true);
     // Gestion des d�placements des Nuages
